4998fb2a99a1d991c9db86d09a4d7cea05cf7561
Rafael Cabral <rafaelkfouri@gmail.com>
2024-09-12 06:32:05 -0300

    adicionei ex matriz e diagonal difference

---
+++ ExMatriz/exMatriz.js
@@ -0,0 +1,15 @@
+var matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+var sDS = 0
+var sDP = 0
+   for(var l = 0; l <= 2; l+=1) {
+        for (var c = 0; c <= 2; c+=1) {
+         
+           sDP = sDP = matriz[l][c]
+    }
+ }
+console.log(sDP)
---
+++ Exercícios-Lógica-Js/HackerRank/Diagonal Difference.js
@@ -0,0 +1,6 @@
+
+
+function diagonalDifference(arr) {
+    // Write your code here
+
+}
